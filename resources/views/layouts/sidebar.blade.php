d3a37455d0f954dc7c9e644a30b2d494afcc3eac
Ahmad Sihabillah <asihab58@gmail.com>
2025-01-28 22:02:29 +0700

    menambah fitur beranda admin

--- resources/views/layouts/sidebar.blade.php
+++ resources/views/layouts/sidebar.blade.php
@@ -25,6 +25,16 @@
 
                 @auth
                     <!-- Data Master Section -->
+                    <!-- Dashboard Menu Admin -->
+                    <li class="nav-item user-panel">
+                        <a href="{{ route('master.data.admin') }}"
+                            class="nav-link {{ request()->routeIs('master.data.admin') ? 'active' : '' }}"
+                            onclick="{{ auth()->check() ? '' : 'return confirmLogin(event);' }}">
+                            <i class="bi bi-speedometer"></i>
+                            <p>Beranda Admin</p>
+                        </a>
+                    </li>
+
                     <li class="nav-header user-panel">DATA MASTER</li>
 
                     <!-- Daftar Transaksi -->
